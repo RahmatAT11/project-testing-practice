56003ab6d22bc55dc08597fd316f8e45f0e0aa5d
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-10-31 12:52:36 +0700

    Add more testing for capitalize function
    if passed more than one argument, it will be
    combined

--- project.js
+++ project.js
@@ -1,6 +1,23 @@
-function capitalize(str) {
-  let newStr = str.at(0).toUpperCase() + str.slice(1, str.length);
-  return newStr;
+function capitalize(...str) {
+  console.log(str);
+  if (str.length === 1) {
+    let newStr = str[0].at(0).toUpperCase() + str[0].slice(1, str[0].length);
+    return newStr;
+  } else {
+    let newCombStr = "";
+    str.forEach((element, index) => {
+      let capStr =
+        element.at(0).toUpperCase() + element.slice(1, element.length);
+
+      if (index === 0) {
+        newCombStr += capStr;
+        return;
+      }
+      newCombStr += " " + capStr;
+    });
+
+    return newCombStr;
+  }
 }
 
 module.exports = { capitalize };
--- project.test.js
+++ project.test.js
@@ -5,4 +5,10 @@ test("Capitalize a string", () => {
     expect(project.capitalize("RAHMAT")).toEqual("RAHMAT");
     expect(project.capitalize("Rahmat")).toEqual("Rahmat");
     expect(project.capitalize("rAHMAT")).toEqual("RAHMAT");
+
+    expect(project.capitalize("rahmat", 'alrafi', 'trikada')).toEqual('Rahmat Alrafi Trikada');
 });
+
+// test("Reverse a string", () => {
+//     expect(project.reverseString("Rahmat")).toEqual("tamhaR");
+// })
